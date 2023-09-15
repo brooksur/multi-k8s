3cf562ff28a7e28fd35ab0e9fbfff877f35c211b
Brooks Benson <brooks@pdrutah.com>
2023-09-14 22:17:13 -0600

    added git shas to deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t brooksbenson03/multi-client -f ./client/Dockerfile ./client
-docker build -t brooksbenson03/multi-server -f ./server/Dockerfile ./server
-docker build -t brooksbenson03/multi-worker -f ./worker/Dockerfile ./worker
-docker push brooksbenson03/multi-client
-docker push brooksbenson03/multi-server
-docker push brooksbenson03/multi-worker
+docker build -t brooksbenson03/multi-client:latest -t brooksbenson03/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t brooksbenson03/multi-server:latest -t brooksbenson03/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t brooksbenson03/multi-worker:latest -t brooksbenson03/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push brooksbenson03/multi-client:latest
+docker push brooksbenson03/multi-server:latest
+docker push brooksbenson03/multi-worker:latest
+
+docker push brooksbenson03/multi-client:$SHA
+docker push brooksbenson03/multi-server:$SHA
+docker push brooksbenson03/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=brooksbenson03/multi-server
-kubectl set image deployments/client-deployment client=brooksbenson03/multi-client
-kubectl set image deployments/worker-deployment worker=brooksbenson03/multi-worker
+kubectl set image deployments/server-deployment server=brooksbenson03/multi-server:$SHA
+kubectl set image deployments/client-deployment client=brooksbenson03/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=brooksbenson03/multi-worker:$SHA
